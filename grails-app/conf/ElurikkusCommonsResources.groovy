f0aebd94d57364a0fb921e1cf8b9e036e78a3c04
Raivo Pöhönen <raivo.gm@gmail.com>
2017-03-15 14:45:28 +0200

    Host Boostrap 4 files

--- grails-app/conf/ElurikkusCommonsResources.groovy
+++ grails-app/conf/ElurikkusCommonsResources.groovy
@@ -8,4 +8,18 @@ modules = {
         // Rename it.
         resource url: [dir: 'css', file: 'elurikkus-cms.css', plugin: 'elurikkus-commons'], disposition: 'defer'
     }
+
+    // Tooltips required by Bootstrap 4.
+    tether {
+        resource url: [dir: 'js', file: 'tether.min.js']
+        resource url: [dir: 'css', file: 'tether.min.css']
+    }
+
+    // Bootstrap 4
+    bootstrap {
+        dependsOn 'tether'
+        resource url: [dir: 'js', file: 'bootstrap.min.js', disposition: 'head']
+        resource url: [dir: 'css', file: 'bootstrap.min.css', attrs: [media: 'screen, projection, print']]
+        resource url: [dir: 'css', file: 'bootstrap-grid.min.css', attrs: [media: 'screen, projection, print']]
+    }
 }
