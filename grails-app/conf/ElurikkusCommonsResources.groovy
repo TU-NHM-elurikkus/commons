fb22de816d664eaeafb93a3ace64e5578cfd88d4
Raivo Pöhönen <raivo.gm@gmail.com>
2017-03-16 16:35:25 +0200

    Give resources IDs

--- grails-app/conf/ElurikkusCommonsResources.groovy
+++ grails-app/conf/ElurikkusCommonsResources.groovy
@@ -1,25 +1,24 @@
 modules = {
     menu {
-        resource url: [dir: 'css', file: 'menu.css', plugin: 'elurikkus-commons'], disposition: 'defer'
+        resource id: 'menu-style', url: [dir: 'css', file: 'menu.css', plugin: 'elurikkus-commons'], disposition: 'defer'
     }
 
     commons {
-        resource url: [dir: 'css', file: 'elurikkus.css', plugin: 'elurikkus-commons'], disposition: 'defer'
-        // Rename it.
-        resource url: [dir: 'css', file: 'elurikkus-cms.css', plugin: 'elurikkus-commons'], disposition: 'defer'
+        resource id: 'elurikkus-ala-style', url: [dir: 'css', file: 'elurikkus.css', plugin: 'elurikkus-commons'], disposition: 'defer'
+        resource id: 'elurikkus-common-style', url: [dir: 'css', file: 'elurikkus-common.css', plugin: 'elurikkus-commons'], disposition: 'defer'
     }
 
     // Tooltips required by Bootstrap 4.
     tether {
-        resource url: [dir: 'js', file: 'tether.min.js']
-        resource url: [dir: 'css', file: 'tether.min.css']
+        resource id: 'tether-script', url: [dir: 'js', file: 'tether.min.js']
+        resource id: 'tether-style', url: [dir: 'css', file: 'tether.min.css']
     }
 
-    // Bootstrap 4
+    // Bootstrap 4.
     bootstrap {
         dependsOn 'tether'
-        resource url: [dir: 'js', file: 'bootstrap.min.js', disposition: 'head']
-        resource url: [dir: 'css', file: 'bootstrap.min.css', attrs: [media: 'screen, projection, print']]
-        resource url: [dir: 'css', file: 'bootstrap-grid.min.css', attrs: [media: 'screen, projection, print']]
+        resource id: 'bootstrap-script', url: [dir: 'js', file: 'bootstrap.min.js', disposition: 'head']
+        resource id: 'bootstrap-style', url: [dir: 'css', file: 'bootstrap.min.css', attrs: [media: 'screen, projection, print']]
+        resource id: 'bootstrap-grid', url: [dir: 'css', file: 'bootstrap-grid.min.css', attrs: [media: 'screen, projection, print']]
     }
 }
