913229bf6a3479f62bf07a81a01a0c2b7fb52c97
timo-piirmann <timurlane5@gmail.com>
2017-09-15 14:12:47 +0300

    Try to persist language through apps [fixes #150898881]

--- grails-app/assets/javascripts/common.js
+++ grails-app/assets/javascripts/common.js
@@ -13,4 +13,30 @@ $(document).ready(function() {
     document.getElementById('burger-close').addEventListener('click', function() {
         burgerPopup.className = 'burger__popup';
     });
+
+    setLanguage();
 });
+
+/*
+* Sets the session language based on the stored value. This is needed to share language settings
+* when moving from one app to another
+*/
+function setLanguage() {
+    var grailsLocale = GLOBAL_LOCALE;
+    var language = localStorage.getItem('language');
+
+    // In case this is the first time setting system wide lang
+    if(!language) {
+        language = grailsLocale;
+    }
+
+    // If language on url then we do nothing
+    if(!window.location.search.includes('lang=')) {
+        // If local storage lang is the same as session then all is well in the world
+        if(language !== grailsLocale) {
+            window.location.search += '&lang=' + language; // Reload the page
+        }
+    } else {
+        localStorage.setItem('language', grailsLocale);
+    }
+}
