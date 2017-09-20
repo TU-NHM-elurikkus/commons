6f45a3e5c9838725e32b6796b5972f2c083ca8e9
timo-piirmann <timurlane5@gmail.com>
2017-09-20 11:27:34 +0300

    Add a general messages controller for js i18n support

--- grails-app/assets/javascripts/common.js
+++ grails-app/assets/javascripts/common.js
@@ -1,7 +1,8 @@
+//= require jquery
+//= require jquery-i18n-properties-1.2.7
 //= require tether.min
 //= require popper.min
 //= require bootstrap.min
-//= require jquery-i18n-properties-1.2.7
 
 $(document).ready(function() {
     $.i18n.properties({
